864f4848bc60a043b432a3b73e61ac9a9e375b3e
Marius Birsan <marius.birsan90@outlook.com>
2023-11-04 16:32:35 +0200

    Exercise 1 was solved

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import {galleryItems} from './gallery-items';
 
-console.log(galleryItems);
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector("ul.gallery");
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element) {
+    return element
+    .map(({preview, original, description}) => {
+        return `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+const galleryHandler =  new SimpleLightbox(".gallery a", {
+    captionsData: 'alt',
+    captionsDelay: 250
+});
+
+galleryHandler.on('show.simplelightbox');
